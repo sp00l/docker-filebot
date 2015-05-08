c8ac4c1d0e6f716617f91e08ad3e41c44fb252ce
Francis Bélanger <fbelanger@lapresse.ca>
2015-05-07 21:41:04 -0400

    Changed build name Move text file to a processed folder afterwards

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --rm -t inkubux/docker-filebot .
+docker build --rm -t inkubux/filebot-inotify .
 
 
--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p /config/scripts
+mkdir -p /watch/processed
+
 cp -u -p /filebot.sh /config/scripts/filebot.sh
 cp -u -p /postprocess.sh /config/scripts/postprocess.sh
 
@@ -9,7 +11,10 @@ chmod u+x /config/scripts/postprocess.sh
 
  inotifywait -m -q -e moved_to,create --format '%f' /watch | while read FILE
  do
-   echo "something happened on path $FILE"
+   echo "New file detected $FILE"
    ./config/scripts/filebot.sh $FILE
    ./config/scripts/postprocess.sh $FILE
+
+   echo "Moving file $FILE to /watch/processed"
+   mv "/watch/$FILE" /watch/processed
  done
