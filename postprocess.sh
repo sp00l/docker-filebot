bdb7d5a52c6d9b8e043f27f2c29f234f0a98c6bf
Francis Bélanger <fbelanger@lapresse.ca>
2015-05-08 07:42:53 -0400

    simple fix

--- postprocess.sh
+++ postprocess.sh
@@ -1,4 +1,4 @@
-echo "Running postprocess... on $0 $1 $2"
 #!/bin/bash
+echo "Running postprocess... on $0 $1 $2"
 # post process-file add any script you want to run when file-bot is done
 # it will receive the name of the file copied.
