9b4a734ee57ec6bbcf17ae39accd2696d1b1eea9
inkubux <francis.belanger@gmail.com>
2016-08-08 11:54:32 -0400

    Update default filebot script
    you can now specify the path to process by writing the path in the "queue" file

--- filebot.sh
+++ filebot.sh
@@ -1,3 +1,10 @@
 echo "Running filebot ...$0 $1 $2"
 
-/usr/bin/filebot -script fn:amc -no-xattr --output "/media" --log-file /config/amc.log --action duplicate --conflict auto -non-strict --def reportError=y "ut_dir=/downloads" "ut_kind=multi" --def clean=y
+PROCESS_PATH=$(cat "/watch/$1")
+if [ "$PROCESS_PATH" = "" ]; then PATH="/downloads"; fi
+
+echo "Path is $PROCESS_PATH"
+
+/usr/bin/filebot -script fn:amc -no-xattr --output "/media" --log-file /config/amc.log --action duplicate --conflict auto -non-strict --def reportError=y "ut_dir=$PROCESS_PATH" "ut_kind=multi" --def clean=y --def excludeList=amc.txt --def "exec=chmod 777 -R '{folder}'"
+
+echo "Done..."
